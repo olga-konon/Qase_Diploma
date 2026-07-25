30304c1393631d6b4622a0d1854303d053b3258a
oljakonon-cmyk/ <olja.konon@gmail.com>
2026-07-25 12:30:12 +0200

    added threads in testng

--- src/main/java/utils/TestDataGenerator.java
+++ src/main/java/utils/TestDataGenerator.java
@@ -1,17 +1,21 @@
 package utils;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class TestDataGenerator {
 
+    private static final AtomicLong COUNTER = new AtomicLong();
+
     public static String generateProjectName() {
-        return "Name-" + System.currentTimeMillis();
+        return "Name-" + System.currentTimeMillis() + COUNTER.incrementAndGet();
     }
 
     public static String generateProjectCode() {
-        return "Code" + (System.currentTimeMillis() % 100000);
-    }
-    public static String generateCaseTitle() {
-        return "Case" + (System.currentTimeMillis() % 100000);
+        return "Code" + (System.currentTimeMillis() % 100000) + COUNTER.incrementAndGet();
     }
 
+    public static String generateCaseTitle() {
+        return "Case" + (System.currentTimeMillis() % 100000) + COUNTER.incrementAndGet();
+    }
 
 }
